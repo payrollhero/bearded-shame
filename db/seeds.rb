15dee8201ef33212a80a8e20b119b68d27137f7b
Carl Tablante <carl.q.tablante@gmail.com>
2015-03-31 15:38:34 +0800

    added code in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(shaving massage haircut farming cutting fighting driving coding trimming typing).each do |talent|
+  Talent.create name: talent
+end
