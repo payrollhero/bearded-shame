6c559777724f87c609a1a09fba2dfb6381fe6874
Carl Tablante <carl.q.tablante@gmail.com>
2015-03-31 13:54:27 +0800

    savepoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  email      :string
+#  username   :string
+#  password   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   def fullname
     first_name + " " + last_name
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,7 +18,7 @@ Rails.application.routes.draw do
   #   resources :products
   #
   #
-  root 'users#list'
+  root 'site#index'
   resources :users do
     member do
       get 'trim'
