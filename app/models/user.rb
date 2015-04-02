2602b108f3547c0f74f1f4e35a6ecff5344fbdbe
Victor <victorpham93@gmail.com>
2015-04-03 00:12:13 +0800

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :beard_length, numericality: { equal_to: 20 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
 
   def fullname
     first_name + " " + last_name
