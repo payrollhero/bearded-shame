76b80d35585c832ac5debbef2a1f9c97b680ffbc
ryanicle <ryanicle@gmail.com>
2015-04-21 16:15:00 +0000

    Add create user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,4 +39,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :beard_length)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,29 @@
 class User < ActiveRecord::Base
+  require 'beard_trimmer'
+  require 'beard_shaver'
+  require 'beard_trimmer/with_massage'
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :beard_length, numericality: { greater_than: 0 }
 
   def fullname
     first_name + " " + last_name
+  end
+
+  # Update user for action that was chosen
+  def self.update_user(id, action)
+    @user = find(id)
+    unless @user.blank?
+      if action == 'trim'
+        return BeardTrimmer.trim(@user, @user.beard_length)
+      elsif action == 'massage_and_trim'
+        return BeardTrimmer::WithMassage.trim(@user, @user.beard_length, 'massage_and_trim')
+      elsif action == 'shave'
+        return BeardShaver.shave(@user)
+      end
+    end
+    return false
   end
 end
