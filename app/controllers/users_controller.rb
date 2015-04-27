00dbf080494681c294ac98429083d342e7b63bae
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-04-27 14:48:23 -0700

    create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,17 @@ class UsersController < ApplicationController
     @users = User.all
   end
   alias :index :list
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to '/'
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+    end
 end
