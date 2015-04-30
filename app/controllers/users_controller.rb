6094db1012f762bcb5fee8eb80cf06f2f3c43582
Dang The Hoai Nam <namproctin@gmail.com>
2015-04-30 21:35:54 +0700

    refactor code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,39 @@
 
 class UsersController < ApplicationController
+  before_action :set_user , except: [:list]
 
   def list
     @users = User.all
   end
 
   def trim
-    @user = User.find(params[:id])
   end
 
   def trimming
-    @user = User.find(params[:id])
     @status = @user.trimming(params[:length])
   end
 
   def massage_and_trim
-    @user = User.find(params[:id])
   end
 
   def massaging_and_trimming
-    @user = User.find(params[:id])
     @status = @user.massaging_and_trimming(params[:length],params[:type])
   end
 
   def shave
-    @user = User.find(params[:id])
   end
 
   def shaving
-    @user = User.find(params[:id])
     @user.shaving
   end
 
   alias :index :list
+
+  private
+
+  def set_user
+    if params[:id].present?
+      @user = User.find(params[:id])
+    end
+  end
 end
