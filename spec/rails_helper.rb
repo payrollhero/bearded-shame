ec69a584c0afc60d70a2020f4f46a8bd620e4c67
Dang The Hoai Nam <namproctin@gmail.com>
2015-05-02 13:10:08 +0700

    fix database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -94,6 +94,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  config.before(:each) do
+    DatabaseCleaner.strategy = :deletion
+  end
+
   config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
