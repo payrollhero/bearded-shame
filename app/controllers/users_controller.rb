387b76c823cecfdcef9c828d88778284d1a33324
Vinoth <a.vinothk@outlook.com>
2015-06-29 23:13:09 +0530

    add specs and user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,37 @@
 class UsersController < ApplicationController
-  before_action :set_user, except: [:index]
-  before_action :prevent_rash_skin, except: [:index]
+  before_action :set_user, except: [:index, :new, :create]
+  before_action :prevent_rash_skin, except: [:index, :new, :create, :edit, :show]
 
   def index
     @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html {redirect_to users_path}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def trim
@@ -22,6 +50,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+  end
 
   def set_user
     @user = User.find_by_id(params[:id])
