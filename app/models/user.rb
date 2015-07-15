27474b61802d15a1d184d73d30cac61c82718368
“Nishant <nishant.goel18@outlook.com>
2015-07-15 15:44:57 +0530

    adding crud routes & functionality for shave, trim & massage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 
-  def fullname
-    first_name + " " + last_name
+  def needs_shave_or_trimming
+    last_shaved_at >= 3 && ["shaved", "trimmed", "massaged_and_trimmed"].include?(status)
+  end
+
+  def last_shaved_at
+    ((Time.now - updated_at) / 60 / 60 / 24).round(1).to_i
   end
 end
