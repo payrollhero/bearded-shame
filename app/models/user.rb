2014e0190123309fc618a40280c44b43463cfde3
“Nishant <nishant.goel18@outlook.com>
2015-07-16 12:32:36 +0530

    fixing bug in creating new user with female user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => {:case_sensitive => false}
   validates :gender, :presence => true, :inclusion => { in: %w(male female)}
-  validates :status, :presence => true, :inclusion => { in: %w(shaved massaged_and_trimmed trimmed unshaved)}
+  validates :status, :presence => true, :inclusion => { in: %w(shaved massaged_and_trimmed trimmed unshaved not_applicable)}
   validate :can_be_marked_unshaved
   validate :validate_shaving_need_based_on_gender
   validate :validate_needs_shave_or_trimming
 
   def can_be_marked_unshaved
     if !self.needs_shave_or_trimming && has_beard?(status)
-      errors.add(:base, "User can't be marked unshaved as he has shaved / trimmed #{ActionController::Base.helpers.time_ago_in_words(self.updated_at)} ago.") if self.status_changed?
+      errors.add(:base, "User can't be marked unshaved as he has shaved / trimmed #{ActionController::Base.helpers.time_ago_in_words(self.shaved_at)} ago.") if self.status_changed?
     end
   end
 
@@ -23,23 +23,29 @@ class User < ActiveRecord::Base
 
   def validate_needs_shave_or_trimming
     if !has_beard?(status_was) && !has_beard?(status) && self.status_changed?
-      errors.add(:base, "User can't be shaved / trimmed again as he has shaved / trimmed #{ActionController::Base.helpers.time_ago_in_words(self.updated_at)} ago.") if !self.needs_shave_or_trimming
+      errors.add(:base, "User can't be shaved / trimmed again as he has shaved / trimmed #{ActionController::Base.helpers.time_ago_in_words(self.shaved_at)} ago.") if !self.needs_shave_or_trimming
     else
       return true
     end
   end
 
   def validate_shaving_need_based_on_gender
-    errors.add(:base, "Female user can't be shaved / trimmed.") if !self.male? 
+    errors.add(:base, "Female user can't be shaved / trimmed.") if !self.male? && status != "not_applicable"
   end
 
   def needs_shave_or_trimming
-    if status_was.present? && last_shaved_at >= 3
+    if ["shaved", "massaged_and_trimmed", "trimmed" "unshaved"].include?(status) && self.male?
+      if status_was.present? && last_shaved_at >= 3
+        return true
+      elsif status_was.present? && last_shaved_at <= 3
+        return false
+      else status_was.nil?
+        return true
+      end
+    elsif status == "not_applicable" && !self.male?
       return true
-    elsif status_was.present? && last_shaved_at <= 3
+    elsif status != "not_applicable" && !self.male?
       return false
-    else status_was.nil?
-      return true
     end
   end
 
