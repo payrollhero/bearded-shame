11d4140b926361ff9c3148ff12f903d7324c490e
Scott Barrow <scott@guestfolio.com>
2015-11-05 20:28:03 -0800

    Use mysql for database

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@
 ActiveRecord::Schema.define(version: 20150330105752) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.string   "email",      limit: 255
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
