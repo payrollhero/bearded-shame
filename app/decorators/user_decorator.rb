8408c62cb7dd3b44713518d28755864242199b8d
Scott Barrow <scott@guestfolio.com>
2015-11-09 18:40:33 -0800

    add class to decorated user actions

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,25 +8,25 @@ class UserDecorator < Draper::Decorator
   end
 
   def link_to_trim
-    h.link_to(h.trim_user_path(object)) do |link|
+    h.link_to(h.new_user_trim_path(object), class: 'trim') do |link|
       h.content_tag(:i, "", class:'fa fa-2x fa-cut')
     end
   end
 
   def link_to_massage_trim
-    h.link_to(h.massage_and_trim_user_path(object)) do |link|
+    h.link_to(h.new_user_massage_trim_path(object), class: 'massage-trim') do |link|
       h.content_tag(:i, "", class:'fa fa-2x fa-bed')
     end
   end
 
   def link_to_shave
-    h.link_to(h.shave_user_path(object)) do |link|
+    h.link_to(h.user_trims_path(object, treatment: {trim_type_id: 1}), class: 'shave', method: :create) do |link|
       h.content_tag(:i, "", class:'fa fa-2x fa-smile-o')
     end
   end
 
   def link_to_delete_user
-    h.link_to(h.user_path(object)) do |link|
+    h.link_to(h.user_path(object), class: 'delete-user', data: { confirm: "Delete user #{object.decorate.full_name}?" }, method: :delete) do
       h.content_tag(:i, "", class:'fa fa-2x fa-trash-o')
     end
   end
