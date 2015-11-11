49e2a306d0f39aa9240ce611d9cdae1d49e971e6
Scott Barrow <scott@guestfolio.com>
2015-11-10 22:21:00 -0800

    beard trimmer service

--- app/services/beard_trimmer.rb
+++ app/services/beard_trimmer.rb
@@ -1,9 +1,20 @@
 # This class trims beards
 class BeardTrimmer
 
-  def self.trim(user, length)
-    user.beard_length = user.beard_length - length
-    user.save
+  def self.call(user: user, type: type)
+    @type = type
+    relation = Hash.new {|h,k| h[k] = @type }
+    relation[@type.class.to_s.underscore]
+    @user  = user
+    @trim  = user.treatments.build(relation)
+
+    trim_facial_hair if type.is_a? TrimType
+    @trim
   end
 
+  def self.trim_facial_hair
+    facial_hair = @user.facial_hair
+    facial_hair.update_attributes(hair_length: @type.length, last_trimmed: Time.now)
+    !false
+  end
 end
